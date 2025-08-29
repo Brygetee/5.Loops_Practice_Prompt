f44bb587fbad9e843814f52d51e42d02619d8dbd
Brygetee Al-Shawkani <brygetee@gmail.com>
2025-08-28 22:28:28 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+5. Loops (for, while)
+What you should know: Repeat actions over a sequence
+ or until a condition is met.
+Practice Prompt: Write a loop that goes through a list of fabric colors
+(red, blue, green) and prints “Color chosen: …” for each.
+
+"""
+
+colors = ["Red","Orange","Yellow","Blue"]
+for color in colors:
+    print(f"Color Chosen: {color}")
+
+"""How to improve the code
+-make sure the code matches the prompt exactly
+"""
+colors = ["red", "blue", "green"]
+
+for color in colors:
+    print(f"Color chosen: {color}")
